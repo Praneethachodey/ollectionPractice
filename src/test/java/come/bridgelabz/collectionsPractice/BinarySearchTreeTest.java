8d7f0d03cc868e5049f9d215e265eb97b6663977
Praneethachodey <praneethachodey@gmail.com>
2020-10-13 00:54:30 +0530

    [Praneetha]Added: test case with 13 nodes in bst

--- src/test/java/come/bridgelabz/collectionsPractice/BinarySearchTreeTest.java
+++ src/test/java/come/bridgelabz/collectionsPractice/BinarySearchTreeTest.java
@@ -6,15 +6,34 @@ import org.junit.Test;
 public class BinarySearchTreeTest {
 
 	@Test
-	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree()
+	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+		Assert.assertSame(3, size);
+	}
+	
+	@Test
+	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen()
 	{
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		int size=myBinaryTree.getSize();
-		Assert.assertSame(3,size);
+		Assert.assertSame(13,size);
 	}
-	
-	
+
 }
